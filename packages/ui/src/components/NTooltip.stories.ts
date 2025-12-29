fb901198f8f5db322a0fb4b351c7b486ea227a80
Gordon C <nevtio.net@gmail.com>
2025-12-29 02:47:27 +0000

    Fix tooltip component

--- packages/ui/src/components/NTooltip.stories.ts
+++ packages/ui/src/components/NTooltip.stories.ts
@@ -1,9 +1,8 @@
-import type { Meta, StoryObj } from '@storybook/vue3-vite'
-// import { fn } from '@storybook/test'
+import type { Meta, StoryObj } from '@storybook/vue3'
 import NTooltip from './NTooltip.vue'
 import NButton from './NButton.vue'
 import NIcon from './NIcon.vue'
-import { ref, useTemplateRef } from 'vue'
+import { ref } from 'vue'
 
 const meta = {
     title: 'UI/NTooltip',
@@ -13,133 +12,159 @@ const meta = {
     },
     tags: ['autodocs'],
     argTypes: {
-        // Props
-    },
-    args: {}
-} satisfies Meta
+        direction: { control: 'select', options: ['top', 'bottom', 'left', 'right'] },
+        position: { control: 'select', options: ['start', '', 'end'] },
+        triggerByHover: { control: 'boolean' },
+        triggerByFocus: { control: 'boolean' },
+        triggerByInteraction: { control: 'boolean' },
+        allowClickToHide: { control: 'boolean' },
+        persistent: { control: 'boolean' },
+        overlay: { control: 'boolean' },
+        fit: { control: 'boolean' }
+    }
+} satisfies Meta<typeof NTooltip>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-    args: {},
     render: args => ({
         components: { NTooltip, NButton },
         setup() {
             return { args }
         },
         template: `
-            <NButton>
-                Show tooltip
-                <NTooltip v-bind="args" >This is a tooltip</NTooltip>
-            </NButton>
-        `
-    })
-}
-
-export const Direction: Story = {
-    args: {},
-    render: args => ({
-        components: { NTooltip, NButton },
-        setup() {
-            return { args }
-        },
-        template: `
-            <div class="flex flex-row items-center gap-4">
-                <NButton>
-                    Top
-                    <NTooltip v-bind="args" direction="top">This is a tooltip</NTooltip>
+            <div class="p-8">
+                <NButton class="brand">
+                    Hover Me
+                    <NTooltip v-bind="args">Simple Tooltip</NTooltip>
                 </NButton>
-                <NButton>
-                    Left
-                    <NTooltip v-bind="args" direction="left">This is a tooltip</NTooltip>
-                </NButton>
-                <NButton>
-                    Right
-                    <NTooltip v-bind="args" direction="right">This is a tooltip</NTooltip>
-                </NButton>
-                <NButton>
-                    Bottom
-                    <NTooltip v-bind="args" direction="bottom">This is a tooltip</NTooltip>
-                </NButton>
-
             </div>
         `
     })
 }
 
-export const Content: Story = {
-    args: {},
+export const ClickToggle: Story = {
+    args: {
+        triggerByHover: false,
+        triggerByInteraction: true,
+        allowClickToHide: true
+    },
+    render: args => ({
+        components: { NTooltip, NButton },
+        setup() {
+            return { args }
+        },
+        template: `
+            <NButton class="brand">
+                Click Me
+                <NTooltip v-bind="args">
+                    Clicked! Click again to close.
+                </NTooltip>
+            </NButton>
+        `
+    })
+}
+
+export const Directions: Story = {
+    render: args => ({
+        components: { NTooltip, NButton },
+        setup() {
+            return { args }
+        },
+        template: `
+            <div class="flex flex-wrap items-center gap-8 p-12">
+                <NButton class="outlined brand">
+                    Top
+                    <NTooltip v-bind="args" direction="top">Tooltip on Top</NTooltip>
+                </NButton>
+                <NButton class="outlined brand">
+                    Bottom
+                    <NTooltip v-bind="args" direction="bottom">Tooltip on Bottom</NTooltip>
+                </NButton>
+                <NButton class="outlined brand">
+                    Left
+                    <NTooltip v-bind="args" direction="left">Tooltip on Left</NTooltip>
+                </NButton>
+                <NButton class="outlined brand">
+                    Right
+                    <NTooltip v-bind="args" direction="right">Tooltip on Right</NTooltip>
+                </NButton>
+            </div>
+        `
+    })
+}
+
+export const RichContent: Story = {
     render: args => ({
         components: { NTooltip, NButton, NIcon },
         setup() {
             return { args }
         },
         template: `
-            <NButton>
-                Show tooltip
-                <NTooltip v-bind="args" >
-                    <h1 class="title-text text-lg">This is a HTML content</h1>
-                    <p>Here is a more advanced example that transitions multiple properties, with different durations and easing curves for enter and leave:</p>
-                    <a href="#" >
-                        Link to somewhere
-                         <NTooltip stacked>
-                            <p class="p-8">Nested tooltip</p>
-                        </NTooltip>                
-                    </a>
-                    
+            <NButton class="brand">
+                HTML Content
+                <NTooltip v-bind="args" class="p-4 w-64 text-left">
+                    <div class="flex flex-col gap-2">
+                        <div class="font-bold flex items-center gap-2">
+                            <NIcon name="mdi-information" class="text-brand" />
+                            Information
+                        </div>
+                        <p class="text-xs">You can include <strong>formatted text</strong>, icons, and even links inside tooltips.</p>
+                        <a href="#" class="text-brand underline text-[10px]">Learn more</a>
+                    </div>
                 </NTooltip>
             </NButton>
         `
     })
 }
 
-export const IconButton: Story = {
-    args: {},
+export const IconButtons: Story = {
     render: args => ({
         components: { NTooltip, NButton },
         setup() {
             return { args }
         },
         template: `
-            <NButton icon="close" class="pilled icon text-xs">
-                <NTooltip v-bind="args" >This is a tooltip</NTooltip>
-            </NButton>
+            <div class="flex items-center gap-4">
+                <NButton icon="mdi-help-circle" class="icon texted brand">
+                    <NTooltip v-bind="args">Get help</NTooltip>
+                </NButton>
+                <NButton icon="mdi-delete" class="icon texted error">
+                    <NTooltip v-bind="args">Delete item</NTooltip>
+                </NButton>
+                <NButton icon="mdi-share" class="icon texted info">
+                    <NTooltip v-bind="args">Share content</NTooltip>
+                </NButton>
+            </div>
         `
     })
 }
 
-export const OverlayButton: Story = {
-    args: {},
+export const PersistentOverlay: Story = {
+    args: {
+        overlay: true,
+        persistent: true,
+        triggerByHover: false,
+        triggerByFocus: false,
+        triggerByInteraction: true
+    },
     render: args => ({
         components: { NTooltip, NButton },
         setup() {
-            const tooltipRef = useTemplateRef<typeof NTooltip>('tooltipRef')
-            const showTooltip = ref(false)
-
-            function handleButtonClick() {
-                // showTooltip.value = !showTooltip.value
-                if (showTooltip.value && tooltipRef.value) tooltipRef.value.hide()
-                else if (!showTooltip.value && tooltipRef.value) tooltipRef.value.show()
-            }
-            return { args, showTooltip, handleButtonClick }
+            const show = ref(false)
+            return { args, show }
         },
         template: `
-            <NButton icon="information-symbol" class="pilled icon text-3xl p-0" @click="handleButtonClick">
-                <NTooltip v-bind="args"
-                    ref="tooltipRef"
-                    v-model="showTooltip"
-                    overlay
-                    persistent
-                    :hoverTriggerAnchor="null"
-                    :focusTriggerAnchor="null"
-                    class="flex flex-col gap-4 items-center"
-                >
-                    <div class="p-4">This is a tooltip</div>
-                    <NButton @click="handleButtonClick">Close</NButton>
+            <div>
+                <NButton class="brand" @click="show = true">Click for Overlay Tooltip</NButton>
+                <NTooltip v-bind="args" v-model="show" class="p-6 flex flex-col items-center gap-4">
+                    <div class="text-lg font-bold">Overlay Tooltip</div>
+                    <p class="text-sm">This tooltip requires a manual close because it's persistent.</p>
+                    <NButton class="brand text-xs" label="Close" @click.stop="show = false" />
                 </NTooltip>
-            </NButton>
+            </div>
         `
     })
 }
--- packages/ui/src/components/NTooltip.vue
+++ packages/ui/src/components/NTooltip.vue
@@ -1,81 +1,54 @@
 <template>
-    <transition v-if="props.stacked" name="n-tooltip">
-        <component
-            :is="props.tag"
-            v-if="model"
-            ref="contentRef"
-            role="tooltip"
-            :style="compStyles"
-            :class="compClasses"
-            v-bind="compBind"
-            @mouseenter="handleContentHoverFocusIn"
-            @mouseleave="handleContentHoverFocusOut"
-            @focusin="handleContentHoverFocusIn"
-            @focusout="handleContentHoverFocusOut"
-        >
-            <span v-if="props.content" v-html="props.content"></span>
-            <template v-for="(node, index) in slotDefaultNodes" v-else :key="index">
-                <component :is="node" />
-            </template>
-        </component>
-    </transition>
-
-    <!-- Overlay -->
-    <teleport v-else-if="isReady && props.overlay" to="#n-tooltips-container">
-        <transition name="n-tooltip-overlay">
-            <div v-if="model" class="n-tooltip-overlay">
-                <component
-                    :is="props.tag"
-                    ref="contentRef"
-                    role="tooltip"
-                    :style="compStyles"
-                    :class="compClasses"
-                    v-bind="compBind"
-                    @mouseenter="handleContentHoverFocusIn"
-                    @mouseleave="handleContentHoverFocusOut"
-                    @focusin="handleContentHoverFocusIn"
-                    @focusout="handleContentHoverFocusOut"
-                >
-                    <span v-if="props.content" v-html="props.content"></span>
-                    <template v-for="(node, index) in slotDefaultNodes" v-else :key="index">
-                        <component :is="node" />
-                    </template>
-                </component>
-            </div>
-        </transition>
-    </teleport>
-
-    <!-- Without Overlay -->
-    <teleport v-else-if="isReady" to="#n-tooltips-container">
+    <template v-if="props.stacked">
         <transition name="n-tooltip">
             <component
                 :is="props.tag"
                 v-if="model"
                 ref="contentRef"
-                role="tooltip"
-                :style="compStyles"
                 :class="compClasses"
+                :role="props.role"
                 v-bind="compBind"
-                @mouseenter="handleContentHoverFocusIn"
-                @mouseleave="handleContentHoverFocusOut"
-                @focusin="handleContentHoverFocusIn"
-                @focusout="handleContentHoverFocusOut"
             >
-                <span v-if="props.content" v-html="props.content"></span>
-                <template v-for="(node, index) in slotDefaultNodes" v-else :key="index">
-                    <component :is="node" />
-                </template>
+                <slot name="default">
+                    <span v-if="props.content" v-html="props.content" />
+                </slot>
+            </component>
+        </transition>
+    </template>
+
+    <teleport v-else-if="isReady" to="#n-tooltips-container">
+        <transition :name="props.overlay ? 'n-tooltip-overlay' : 'n-tooltip'">
+            <div v-if="model && props.overlay" class="n-tooltip-overlay" aria-hidden="true">
+                <component :is="props.tag" ref="contentRef" :class="compClasses" :role="props.role" v-bind="compBind">
+                    <slot name="default">
+                        <span v-if="props.content" v-html="props.content" />
+                    </slot>
+                </component>
+            </div>
+            <component
+                :is="props.tag"
+                v-else-if="model"
+                ref="contentRef"
+                :class="compClasses"
+                :role="props.role"
+                v-bind="compBind"
+            >
+                <slot name="default">
+                    <span v-if="props.content" v-html="props.content" />
+                </slot>
             </component>
         </transition>
     </teleport>
 </template>
 
 <script setup lang="ts">
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    /* eslint-disable @typescript-eslint/no-unused-vars, no-unused-vars */
     import type { Placement } from '@floating-ui/vue'
-    import { computed, HTMLAttributes, onMounted, ref, useAttrs, useSlots, useTemplateRef } from 'vue'
+    import { computed, type HTMLAttributes, onMounted, ref, useAttrs, useTemplateRef } from 'vue'
     import { useFloating } from '../composables/use-floating'
     import { useTeleportContainer } from '../composables/use-teleport-container'
-    import { getElement, getParentElement, wrapTextNode } from '../helpers/dom'
+    import { getElement, getParentElement } from '../helpers/dom'
 
     export type NTooltipDirection = 'top' | 'bottom' | 'left' | 'right'
     export type NTooltipPosition = 'start' | '' | 'end'
@@ -87,7 +60,12 @@
         persistent?: boolean
         hoverTriggerAnchor?: HTMLElement | string | null
         focusTriggerAnchor?: HTMLElement | string | null
+        clickTriggerAnchor?: HTMLElement | string | null
         attachParent?: HTMLElement | string | null
+        triggerByHover?: boolean
+        triggerByFocus?: boolean
+        triggerByInteraction?: boolean
+        allowClickToHide?: boolean
         direction?: NTooltipDirection
         position?: NTooltipPosition
         margin?: number
@@ -96,13 +74,13 @@
         stacked?: boolean
         overlay?: boolean
         fit?: boolean
+        role?: string
     }
 
     defineOptions({
         inheritAttrs: false
     })
 
-    const slots = useSlots()
     const attrs = useAttrs()
     const props = withDefaults(defineProps<NTooltipProps>(), {
         tag: 'span',
@@ -110,6 +88,10 @@
         showDelay: 75,
         hideDelay: 250,
         persistent: false,
+        triggerByHover: true,
+        triggerByFocus: true,
+        triggerByInteraction: false,
+        allowClickToHide: false,
         direction: 'bottom',
         position: '',
         margin: 8,
@@ -117,7 +99,8 @@
         autoReposition: true,
         stacked: false,
         overlay: false,
-        fit: false
+        fit: false,
+        role: 'tooltip'
     })
 
     const model = defineModel<boolean>({ default: false })
@@ -125,14 +108,18 @@
     const { isReady } = useTeleportContainer('n-tooltips-container')
 
     const parentEl = ref<HTMLElement | null>(null)
-    const attachParentEl = computed<HTMLElement | null>(() =>
-        props.attachParent ? getElement(props.attachParent) : parentEl.value
-    )
+    const attachParentEl = computed(() => (props.attachParent ? getElement(props.attachParent) : parentEl.value))
 
-    const floatingPlacement = computed<Placement>(() => {
-        const placement = `${props.direction}${props.position !== '' ? `-${props.position}` : ''}` as Placement
-        return placement
+    const floatingPlacement = computed(() => {
+        return `${props.direction}${props.position !== '' ? `-${props.position}` : ''}` as Placement
     })
+
+    const floatingProps = computed(() => ({
+        ...props,
+        hoverTriggerAnchor: props.triggerByHover ? props.hoverTriggerAnchor : null,
+        focusTriggerAnchor: props.triggerByFocus ? props.focusTriggerAnchor : null,
+        clickTriggerAnchor: props.triggerByInteraction ? props.clickTriggerAnchor : null
+    }))
 
     const {
         show,
@@ -142,10 +129,10 @@
         compStyles: floatingStyles,
         placement,
         parentWidth
-    } = useFloating(props, {
-        model: computed<boolean>({
+    } = useFloating(floatingProps, {
+        model: computed({
             get: () => model.value,
-            set: (val: boolean) => {
+            set: val => {
                 model.value = val
             }
         }),
@@ -162,25 +149,51 @@
         return styles
     })
 
-    const compClasses = computed(() => {
-        return ['n-tooltip', `n-tooltip--direction-${placement.value}`]
-    })
+    const compClasses = computed(() => ['n-tooltip', `n-tooltip--direction-${placement.value}`])
+
     const compBind = computed(() => {
+        const {
+            tag,
+            content,
+            showDelay,
+            hideDelay,
+            persistent,
+            hoverTriggerAnchor,
+            focusTriggerAnchor,
+            clickTriggerAnchor,
+            attachParent,
+            triggerByHover,
+            triggerByFocus,
+            triggerByInteraction,
+            allowClickToHide,
+            direction,
+            position,
+            margin,
+            offset,
+            autoReposition,
+            stacked,
+            overlay,
+            fit,
+            role,
+            ...rest
+        } = props
+
         return {
+            style: compStyles.value,
+            onMouseenter: handleContentHoverFocusIn,
+            onMouseleave: handleContentHoverFocusOut,
+            onFocusin: handleContentHoverFocusIn,
+            onFocusout: handleContentHoverFocusOut,
+            ...(rest as any),
             ...attrs
         }
     })
 
-    const slotDefaultNodes = computed(() => {
-        return wrapTextNode(slots.default?.() ?? [], 'span')
-    })
-
-    // Lifecycle hooks
     onMounted(() => {
         parentEl.value = getParentElement()
     })
 
-    defineExpose({ show, hide })
+    defineExpose({ show, hide, contentRef })
 </script>
 
 <style lang="css">
@@ -193,7 +206,7 @@
 
             &.n-tooltip-overlay-enter-active,
             &.n-tooltip-overlay-leave-active {
-                @apply transition-[opacity,translate] duration-200 ease-in-out;
+                @apply transition-opacity duration-200 ease-in-out;
             }
             &.n-tooltip-overlay-leave-active {
                 @apply delay-200;
